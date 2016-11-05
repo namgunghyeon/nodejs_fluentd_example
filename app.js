43bf02667af2a91d613b39b78a3ecb7bd59f069a
nkh <nkh@mac88ui-iMac.local>
2016-11-05 23:25:25 +0900

    remove comment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ logger.configure('app.follow', {
    host: '192.168.56.116',
    port: 24224,
    timeout: 3.0,
-   reconnectInterval: 600000 // 10 minutes
+   reconnectInterval: 600000
 });
 
 logger.emit('label', {record: 'this is a log'});
